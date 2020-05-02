1ffb64c7af25f5a749741d9f4152fe9b7acf643d
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-05-02 17:21:24 +0530

    Create Java Date and Time.java

---
+++ Java Date and Time.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import java.time.LocalDate;                                   //for LocalDate func
+
+class Result 
+{
+    public static String findDay(int month, int day, int year) 
+    {
+        return LocalDate.of(year, month, day).getDayOfWeek().name();   //to get the day name 
+    }
+}
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+/*
+Input Format
+
+A single line of input containing the space separated month, day and year, respectively, in    format.
+
+
+Output the correct day in capital letters.
+
+Sample Input
+  08 05 2015
+
+Sample Output
+  WEDNESDAY
+
+*/
